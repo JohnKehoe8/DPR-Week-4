c183cb06218561f956d77936591d3e666c03d6d6
JohnKehoe8 <89929114+JohnKehoe8@users.noreply.github.com>
2021-09-22 11:53:02 -0400

    Update car.py

--- car.py
+++ car.py
@@ -2,7 +2,7 @@ cars = 100
 space_in_a_car = 4.0
 drivers = 30
 passengers = 30
-cars_not_Driven = cars - drivers
+cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 #explain comments
@@ -10,7 +10,7 @@ average_passengers_per_car = passengers / cars_driven
 
 print ("There are", cars, "cars available.")
 print ("There are only", drivers, "drivers available.")
-print "There will be", cars_not_driven, "empty cars today.")
+print ("There will be", cars_not_driven, "empty cars today.")
 print ("We can transport", carpool_capacity, "people today.")
-print ("We have", passenger, "to carpool today.")
+print ("We have", passengers, "to carpool today.")
 print ("We need to put about", average_passengers_per_car, "in each car.")
